209c2855f9f734b6aed61af43b0aa1bfa9d292cf
Mariana Duarte <guilhermespedro@outlook.com>
2019-09-24 11:53:33 +0100

    LIS-FTWD-Guilherme#156

--- starter-code/src/App.jsx
+++ starter-code/src/App.jsx
@@ -1,21 +1,67 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-
+import contacts from "./contacts.json";
 import "./App.css";
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.state.contacts = contacts.slice(0, 5);
+    this.addRandom = this.addRandom.bind(this);
+    this.deleteContact = this.deleteContact.bind(this);
+  }
+  makeRow(contact, key) {
+    return (
+      <tr key={key}>
+        <td>
+          <img src={contact.pictureUrl} width="100px" alt="" />
+        </td>
+        <td>{contact.name}</td>
+        <td>{contact.popularity.toFixed(2)}</td>
+        <td>
+          <button
+            onClick={() => {
+              this.deleteContact(key);
+            }}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  }
+  addRandom() {
+    let randomIndex = Math.floor(Math.random() * contacts.length);
+    this.setState({
+      contacts: [...this.state.contacts, contacts[randomIndex]]
+    });
+  }
+  deleteContact(key) {
+    const newArray = this.state.contacts.slice();
+    newArray.splice(key, 1);
+    this.setState({ contacts: newArray });
+  }
   render() {
-    const message = "This is my app";
-
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{message}</p>
-        </header>
+        <h1>IronContacts</h1>
+        <button onClick={this.addRandom}>Add random contact</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contacts.map((contact, index) => {
+              return this.makeRow(contact, index);
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
-
 export default App;
